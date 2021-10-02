162339cfb0ce022a800b6d5c2edae16449cdeed6
Guled Farah <guled13s@gmail.com>
2021-10-02 19:18:53 -0400

    Added AutoMapper for ProductDto <=> Product
    Added AutoMapper
    Registered Mapper configuations as Singleton instance

---
+++ Transauto.Services.ProductAPI/MappingConfig.cs
@@ -0,0 +1,23 @@
+﻿using AutoMapper;
+using Transauto.Services.ProductAPI.Models;
+using Transauto.Services.ProductAPI.Models.Dtos;
+
+namespace Transauto.Services.ProductAPI
+{
+    public class MappingConfig
+    {
+        #region Public Methods
+
+        public static MapperConfiguration RegisterMaps()
+        {
+            var mappingConfig = new MapperConfiguration(config =>
+            {
+                config.CreateMap<ProductDto, Product>().ReverseMap();
+            });
+
+            return mappingConfig;
+        }
+
+        #endregion Public Methods
+    }
+}
--- Transauto.Services.ProductAPI/Startup.cs
+++ Transauto.Services.ProductAPI/Startup.cs
@@ -1,41 +1,34 @@
+using AutoMapper;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Transauto.Services.ProductAPI.DbContexts;
 
 namespace Transauto.Services.ProductAPI
 {
     public class Startup
     {
+        #region Public Constructors
+
         public Startup(IConfiguration configuration)
         {
             Configuration = configuration;
         }
 
+        #endregion Public Constructors
+
+        #region Public Properties
+
         public IConfiguration Configuration { get; }
 
-        // This method gets called by the runtime. Use this method to add services to the container.
-        public void ConfigureServices(IServiceCollection services)
-        {
-            services.AddDbContext<ApplicationDbContext>(options =>
-            options.UseSqlServer(Configuration.GetConnectionString("ProductsAPIConnection")));
-            services.AddControllers();
-            services.AddSwaggerGen(c =>
-            {
-                c.SwaggerDoc("v1", new OpenApiInfo { Title = "Transauto.Services.ProductAPI", Version = "v1" });
-            });
-        }
+        #endregion Public Properties
+
+        #region Public Methods
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
@@ -58,5 +51,26 @@ namespace Transauto.Services.ProductAPI
                 endpoints.MapControllers();
             });
         }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            //Add DbContext and DbConext Configurations
+            services.AddDbContext<ApplicationDbContext>(options =>
+            options.UseSqlServer(Configuration.GetConnectionString("ProductsAPIConnection")));
+            //Add Automapper and Automapper configuration
+            IMapper mapper = MappingConfig.RegisterMaps().CreateMapper();
+            //Register Mapper instance as a Singleton
+            services.AddSingleton(mapper);
+            //Load current domain assemblies to the AutoMapper
+            services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
+            services.AddControllers();
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "Transauto.Services.ProductAPI", Version = "v1" });
+            });
+        }
+
+        #endregion Public Methods
     }
 }
