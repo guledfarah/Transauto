8cb6edab0ad857a05dca47d3523e1c1f5d88a2eb
Guled Farah <guled13s@gmail.com>
2021-10-02 17:06:13 -0400

    added ProductAPI DbContext
    Added:
    - ConnectionString
    - DbContext
    - Product Model
    - Migrations

---
+++ Transauto.Services.ProductAPI/DbContexts/ApplicationDbContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Transauto.Services.ProductAPI.Models;
+
+namespace Transauto.Services.ProductAPI.DbContexts
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Product> Products { get; set; }
+    }
+}
