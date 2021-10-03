2adc04d108c997291b319a9b7f41b4219ce049f2
Guled Farah <guled13s@gmail.com>
2021-10-02 21:08:30 -0400

    Added migrations for the Product Seed

--- Transauto.Services.ProductAPI/DbContexts/ApplicationDbContext.cs
+++ Transauto.Services.ProductAPI/DbContexts/ApplicationDbContext.cs
@@ -32,7 +32,7 @@ namespace Transauto.Services.ProductAPI.DbContexts
                 Price = 12.4,
                 Description = "fried triangular shaped pastry with a savory filling like spiced onions, beef meat, and other ingredients",
                 CategoryName = "Entree",
-                ImageUrl = ""
+                ImageUrl = "https://transautostatifiles.blob.core.windows.net/productimages/sambusa.jpg"
             });
 
             modelBuilder.Entity<Product>().HasData(new Product
@@ -42,7 +42,7 @@ namespace Transauto.Services.ProductAPI.DbContexts
                 Price = 2.0,
                 Description = "a layered pastry dessert made of filo pastry, filled with chopped nuts, and sweetened with syrup or honey",
                 CategoryName = "Dessert",
-                ImageUrl = ""
+                ImageUrl = "https://transautostatifiles.blob.core.windows.net/productimages/baklava.jpg"
             });
         }
 
---
+++ Transauto.Services.ProductAPI/Migrations/20211003010752_SeedProducts.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Transauto.Services.ProductAPI.Migrations
+{
+    public partial class SeedProducts : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Products",
+                columns: new[] { "ProductId", "CategoryName", "Description", "ImageUrl", "Name", "Price" },
+                values: new object[] { 1, "Entree", "fried triangular shaped pastry with a savory filling like spiced onions, beef meat, and other ingredients", "https://transautostatifiles.blob.core.windows.net/productimages/sambusa.jpg", "Sambusa", 12.4 });
+
+            migrationBuilder.InsertData(
+                table: "Products",
+                columns: new[] { "ProductId", "CategoryName", "Description", "ImageUrl", "Name", "Price" },
+                values: new object[] { 2, "Dessert", "a layered pastry dessert made of filo pastry, filled with chopped nuts, and sweetened with syrup or honey", "https://transautostatifiles.blob.core.windows.net/productimages/baklava.jpg", "Baklava", 2.0 });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Products",
+                keyColumn: "ProductId",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Products",
+                keyColumn: "ProductId",
+                keyValue: 2);
+        }
+    }
+}
