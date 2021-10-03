ca874a360b4928a8206e0a6f23a09e41a23888d1
Guled Farah <guled13s@gmail.com>
2021-10-02 20:34:38 -0400

    Added DbContext seed for the Product Table

--- Transauto.Services.ProductAPI/DbContexts/ApplicationDbContext.cs
+++ Transauto.Services.ProductAPI/DbContexts/ApplicationDbContext.cs
@@ -18,5 +18,34 @@ namespace Transauto.Services.ProductAPI.DbContexts
         public DbSet<Product> Products { get; set; }
 
         #endregion Public Properties
+
+        #region Protected Methods
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<Product>().HasData(new Product
+            {
+                ProductId = 1,
+                Name = "Sambusa",
+                Price = 12.4,
+                Description = "fried triangular shaped pastry with a savory filling like spiced onions, beef meat, and other ingredients",
+                CategoryName = "Entree",
+                ImageUrl = ""
+            });
+
+            modelBuilder.Entity<Product>().HasData(new Product
+            {
+                ProductId = 2,
+                Name = "Baklava",
+                Price = 2.0,
+                Description = "a layered pastry dessert made of filo pastry, filled with chopped nuts, and sweetened with syrup or honey",
+                CategoryName = "Dessert",
+                ImageUrl = ""
+            });
+        }
+
+        #endregion Protected Methods
     }
 }
