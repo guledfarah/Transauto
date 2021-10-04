6c846c8c80c2881d56047e4f656412157d62be80
Guled Farah <guled13s@gmail.com>
2021-10-03 21:12:06 -0400

    Added Identity Web App with Duende Identity Server
    - All our microservices will use Duende Software--IdentityServer for AuthN and AuthZ.

---
+++ Transauto.Services.Identity/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Transauto.Services.Identity.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
